f1fc9958f97a1d5cc5183f69a4f8c7e2e001cd68
bjohnston <bjohnston@saratechinc.com>
2014-09-08 16:42:01 -0700

    Initial Commit of SalesforceConsole

---
+++ SalesforceConsole/Program.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using enterprise = SalesforceConsole.SfdcReference;
+
+namespace SalesforceConsole
+{
+    class Program
+    {
+     
+        public static void Main(string[] args)
+        {
+            SalesforceInfo SFInfo = new SalesforceInfo();
+            SFInfo.ConsumerKey = "****Consumer Key Here****";
+            SFInfo.ConsumerSecret = "****Consumer Secret Here****";
+            SFInfo.UserName = "****Username Here****";
+            SFInfo.Password = "****Salesforce Password Here****";
+            SFInfo.Token = "****Salesforce App Token Here****";
+            
+            List<string> dashboardIDs = new List<string>();
+            dashboardIDs.Add("*******List Dashboard IDs Here********");
+
+            Task SFLogin = SFInfo.Login();
+            SFLogin.Wait();
+
+            Task SFRefresh = SFInfo.RefreshDashboard(dashboardIDs);
+            SFRefresh.Wait();
+
+            Console.WriteLine("Done Refreshing Dashboards.");
+            Console.WriteLine("Bye Bye!");
+        }
+        
+    }
+}
---
+++ SalesforceConsole/Salesforce.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+public class SalesforceInfo
+{
+    public string ConsumerKey;
+    public string ConsumerSecret;
+    public string UserName;
+    public string Password;
+    public string Token;
+    public string oauthToken;
+    public string sessionID;
+    public string serviceURL;
+    public string loginPassword;
+    public async Task<string> Login(){
+        loginPassword =  Password + Token;
+        
+        Console.WriteLine("Logging in to Salesforce...");
+
+        HttpClient authClient = new HttpClient();
+        HttpContent content = new FormUrlEncodedContent(new Dictionary<string, string>
+              {
+                 {"grant_type","password"},
+                 {"client_id",ConsumerKey},
+                 {"client_secret",ConsumerSecret},
+                 {"username",UserName},
+                 {"password",loginPassword}
+               }
+        );
+        HttpResponseMessage message = await authClient.PostAsync("https://login.salesforce.com/services/oauth2/token", content);
+        string responseString = await message.Content.ReadAsStringAsync();
+        JObject obj = JObject.Parse(responseString);
+        oauthToken = (string)obj["access_token"];
+        serviceURL = (string)obj["instance_url"];
+
+        Console.WriteLine("Logged In.");
+
+        return "Finished Login";
+    }
+    public async Task<string> RefreshDashboard(List<string> dashboardIDs) {
+        foreach (string dashboard in dashboardIDs)
+        {
+            Console.WriteLine("Refreshing Dashboard - "+dashboard);
+            HttpClient queryClient = new HttpClient();
+            string restQuery = serviceURL + "/services/data/v31.0/analytics/dashboards/" + dashboard;
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Put, restQuery);
+            request.Headers.Add("Authorization", "Bearer " + oauthToken);
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = await queryClient.SendAsync(request);
+            string result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("Dashboard " + dashboard + " Refreshed");
+        }
+        return "Refreshed!";
+    }
+    
+}
